2069605cf6b2c7310bdea3da75f6220cd8f61ca7
pepperbit <pepperbit@yahoo.com>
2020-09-23 10:31:51 +0300

    Updates

---
+++ precache-manifest.4124e88fa65fd7d53a8f188f01f66100.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a883840a758eeee37c64977bbca21d",
+    "url": "/vp-poc/index.html"
+  },
+  {
+    "revision": "2052631a9fbd62fd6892",
+    "url": "/vp-poc/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "91b360b2963beec47185",
+    "url": "/vp-poc/static/js/2.f7334276.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "/vp-poc/static/js/2.f7334276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2052631a9fbd62fd6892",
+    "url": "/vp-poc/static/js/main.adc93f35.chunk.js"
+  },
+  {
+    "revision": "193244b0c115fc47a6c7",
+    "url": "/vp-poc/static/js/runtime-main.2d265f57.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vp-poc/static/media/logo.5d5d9eef.svg"
+  }
+]);
